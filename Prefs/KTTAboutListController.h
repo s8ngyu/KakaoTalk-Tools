e0bc8969e87aeaf3d81bd59e8dd685e7eaf75e25
peterprd <41620322+peterprd@users.noreply.github.com>
2018-09-25 13:34:04 +0900

    fixed

--- Prefs/KTTAboutListController.h
+++ Prefs/KTTAboutListController.h
@@ -1,12 +1,12 @@
 #import <Preferences/PSListController.h>
 #import <Preferences/PSSpecifier.h>
-#import <CepheiPrefs/HBRootListController.h>
+#import <CepheiPrefs/HBAboutListController.h>
 #import <CepheiPrefs/HBAppearanceSettings.h>
 #import <Cephei/HBPreferences.h>
 #import <spawn.h>
 
 
-@interface KTTAboutListController : HBRootListController
+@interface KTTAboutListController : HBAboutListController
 
 
 @end
